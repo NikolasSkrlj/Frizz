dd7a235c46c09af3b1745e07afcef9d22a23d31d
nikolassas <nikolas.skrlj@hotmail.com>
2020-08-30 21:51:45 +0200

    deploy fix numero tres

--- src/app.js
+++ src/app.js
@@ -14,9 +14,9 @@ app.use(express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   app.get("*", (req, res) => {
-    res.sendfile(path.join((__dirname = "client/build/index.html")));
+    res.sendfile(path.join((__dirname = "../client/build/index.html")));
   });
 } else {
   const morgan = require("morgan");
