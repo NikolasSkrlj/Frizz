5e4ebb8cc2bec5315f68b4d163bbb077836b43e2
nikolassas <nikolas.skrlj@hotmail.com>
2020-08-30 21:36:27 +0200

    deploy fix numero uno

--- src/app.js
+++ src/app.js
@@ -12,13 +12,14 @@ const userRouter = require("./routes/userRouter");
 
 app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"));
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
   app.get("*", (req, res) => {
     res.sendfile(path.join((__dirname = "client/build/index.html")));
   });
+} else {
+  app.use(morgan("dev"));
 }
 
 app.use("/hairsalon", salonRouter);
