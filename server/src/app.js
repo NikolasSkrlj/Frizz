62b591dd7ee0af9c201837d36304b8252060f0ef
Nikolas Skrlj <nikolas.skrlj@hotmail.com>
2023-03-15 11:41:09 +0100

    fixes

--- server/src/app.js
+++ server/src/app.js
@@ -13,29 +13,29 @@ const userRouter = require("./routes/userRouter");
 app.use(express.json());
 app.use(cors());
 //ovo mora biti iznad ovog ispod idk why al bez toga u productionu ne rade requesti get
-app.use("/hairsalon", salonRouter);
-app.use("/user", userRouter);
+app.use("/api/hairsalon", salonRouter);
+app.use("/api/user", userRouter);
 
 const checkAppointments = require("./tasks/appointments");
 
 checkAppointments.start();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(globalPath, "../client/build")));
+  app.use(express.static(path.join(globalPath, "../../client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(globalPath, "../client/build/index.html"));
+    res.sendFile(path.join(globalPath, "../../client/build/index.html"));
   });
 } else {
   const morgan = require("morgan");
   app.use(morgan("dev"));
 }
 
-/* app.post("/", function (req, res) {
-  res.send("POST request to the homepage");
+app.post("/", function (req, res) {
+  res.send({ message: "API is up and running :)" });
 });
 
 app.get("/", function (req, res) {
-  res.send("GET request to the homepage");
-}); */
+  res.send({ message: "API is up and running :)" });
+});
 
 module.exports = app;
